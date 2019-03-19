2c9d3fde42c8ab00592f1732e8cb9aefa7260021
Mikail Fener <mika1292@edu.easj.dk>
2019-03-20 00:48:28 +0100

    Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,30 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let bilType = <HTMLSelectElement>document.getElementById("bilType")
+let pris = <HTMLInputElement> document.getElementById("pris")
+let tekst = <HTMLParagraphElement> document.getElementById("tekst")
+
+document.getElementById("myBtn").addEventListener("click", BeregnAfgift)
+
+function BeregnAfgift()
+{
+    if (bilType.value == "PersonBil") 
+    {
+        tekst.innerText = "Bilafgift: " + BilAfgift();
+    }
+
+    if (bilType.value == "ElBil")
+    {
+        tekst.innerText = "Bilafgift: " + BilAfgift() * 0.2;
+    } 
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function BilAfgift()
+{
+    if(pris.valueAsNumber > 200000)
+    {
+        return pris.valueAsNumber * 1.5 - 130000
+    }
+    else
+    {
+        return pris.valueAsNumber * 0.85;
+    }
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
