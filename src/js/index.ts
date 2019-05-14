e5c7089f48826af771e2ce93a9834d64f78919c8
basicxd <mikail-fener@hotmail.com>
2019-05-14 14:01:01 +0200

    fuvj

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,30 +1,58 @@
-let bilType = <HTMLSelectElement>document.getElementById("bilType")
-let pris = <HTMLInputElement> document.getElementById("pris")
-let tekst = <HTMLParagraphElement> document.getElementById("tekst")
+import axios, {AxiosResponse, AxiosError} from "../../node_modules/axios/index";
+import {ISensor} from "../js/ISensor"
 
-document.getElementById("myBtn").addEventListener("click", BeregnAfgift)
+const uri:string = "https://xn--restndopkald20190514095809-zwc.azurewebsites.net/api/n%C3%B8dopkald/"
 
-function BeregnAfgift()
-{
-    if (bilType.value == "PersonBil") 
-    {
-        tekst.innerText = "Bilafgift: " + BilAfgift();
+let divElement:HTMLDivElement = <HTMLDivElement> document.getElementById("content")
+
+
+function getAllSensor():void{
+    
+    axios.get<ISensor[]>(uri)
+    .then(function(repsonse:AxiosResponse<ISensor[]>):void{
+
+        let olElement:HTMLOListElement = document.createElement('ol');
+
+        let x:number = 0;
+
+        repsonse.data.forEach((sensor:ISensor) => 
+        {
+            x++
+            if(sensor == null)
+                {  
+                    olElement.appendChild(CreateLiElement("NULL element", "error", x));
+                }
+            else
+                {
+                    let tekst:string = "Dato: "+sensor.dato+" Tid: "+sensor.tid+" Motion: "+sensor.motion;
+                    olElement.appendChild(CreateLiElement(tekst, "r1", sensor.id));
+                }
+        });
+
+        if (divElement.firstChild)
+        divElement.removeChild(divElement.firstElementChild);
+      
+        divElement.appendChild(olElement);
     }
-
-    if (bilType.value == "ElBil")
-    {
-        tekst.innerText = "Bilafgift: " + BilAfgift() * 0.2;
-    } 
+    )
+    .catch(function (error:AxiosError):void{
+            divElement.innerHTML= error.message;
+    })
 }
 
-function BilAfgift()
-{
-    if(pris.valueAsNumber > 200000)
-    {
-        return pris.valueAsNumber * 1.5 - 130000
-    }
-    else
-    {
-        return pris.valueAsNumber * 0.85;
-    }
+function CreateLiElement(tekst:string, classAttribut:string, id: number) : HTMLLIElement{
+    
+    let newLiElement = document.createElement('li');
+    let newText = document.createTextNode(tekst)
+    
+    newLiElement.setAttribute('class',classAttribut);
+    newLiElement.setAttribute('id',id.toString());
+    
+    newLiElement.appendChild(newText);
+
+    return newLiElement;
 }
+
+
+
+
